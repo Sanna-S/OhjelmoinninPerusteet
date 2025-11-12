6049256830657239886f3725bfae0c88a2efabb3
Sanna-S <sanna.sjoblad@centria.fi>
2025-11-12 17:33:35 +0200

    .

--- Viikko3/lue_varaukset.py
+++ Viikko3/lue_varaukset.py
@@ -4,15 +4,15 @@ def hae_varaaja(varaus):
     nimi = varaus[1]
     print(f"Varaaja: {nimi}")
 
-    def main():
-        varaukset = "varaukset.txt"
+def main():
+    varaukset = "varaukset.txt"
 
-        with open(varaukset, "r", encoding="utf-8") as f:
-            varaus = f.read().strip()
-            varaus = varaus.split('|')
+    with open(varaukset, "r", encoding="utf-8") as f:
+        varaus = f.read().strip()
+        varaus = varaus.split('|')
 
-            hae_varaaja(varaus)
+        hae_varaaja(varaus)
             
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
