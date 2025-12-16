7b7715df9fd26352ded598d131d43df3b56ae9b7
Sanna-S <sanna.sjoblad@centria.fi>
2025-12-16 12:57:11 +0200

    korjaus

--- Viikko7/lue_varaukset.py
+++ Viikko7/lue_varaukset.py
@@ -5,6 +5,15 @@
 from datetime import datetime
 
 def muunna_varaustiedot(varaus_lista: list[str]) -> dict:
+    """
+    Muuntaa varauslistan sanakirjaksi oikeilla tietotyypeillä.
+    
+    Parametrit:
+    varaus_lista (list[str]): Lista, joka sisältää varauksen tiedot merkkijonoina.
+
+    Palauttaa:
+    dict: Sanakirja, joka sisältää muunnetut varauksen tiedot.
+    """
     return {
         "Id": int(varaus_lista[0]),
         "Nimi": varaus_lista[1],
@@ -20,6 +29,15 @@ def muunna_varaustiedot(varaus_lista: list[str]) -> dict:
     }
 
 def hae_varaukset(varaustiedosto: str) -> list[dict]:
+    """
+    Lukee varaukset tiedostosta varaukset.txt ja palauttaa ne listana sanakirjoja.
+
+    Parametrit:
+    varaustiedosto (str): Tiedostonimi, josta varaukset luetaan.
+
+    Palauttaa:
+    list[dict]: Lista, joka sisältää varaukset sanakirjoina.
+    """
     varaukset = []
     with open(varaustiedosto, "r", encoding="utf-8") as f:
         for varaus in f:
@@ -29,6 +47,12 @@ def hae_varaukset(varaustiedosto: str) -> list[dict]:
     return varaukset
 
 def vahvistetut_varaukset(varaukset: list[dict]):
+    """
+    Tulostaa vahvistetut varaukset.
+
+    Parametrit:
+    varaukset (list[dict]): Lista, joka sisältää varaukset sanakirjoina.
+    """
     for varaus in varaukset:
         if(varaus['Vahvistettu']):
             print(f"- {varaus['Nimi']}, {varaus['Tila']}, {varaus['Paiva'].strftime('%d.%m.%Y')} klo {varaus['Kellonaika'].strftime('%H.%M')}")
@@ -36,6 +60,9 @@ def vahvistetut_varaukset(varaukset: list[dict]):
     print()
 
 def pitkat_varaukset(varaukset: list[dict]):
+    """
+    Tulostaa varaukset, joiden kesto on vähintään 3 tuntia.
+    """
     for varaus in varaukset:
         if(varaus['Kesto'] >= 3):
             print(f"- {varaus['Nimi']}, {varaus['Paiva'].strftime('%d.%m.%Y')} klo {varaus['Kellonaika'].strftime('%H.%M')}, kesto {varaus['Kesto']} h, {varaus['Tila']}")
@@ -43,6 +70,9 @@ def pitkat_varaukset(varaukset: list[dict]):
     print()
 
 def varausten_vahvistusstatus(varaukset: list[dict]):
+    """
+    Tulostaa varausten vahvistusstatuksen.
+    """
     for varaus in varaukset:
         if(varaus['Vahvistettu']):
             print(f"{varaus['Nimi']} → Vahvistettu")
@@ -52,6 +82,9 @@ def varausten_vahvistusstatus(varaukset: list[dict]):
     print()
 
 def varausten_lkm(varaukset: list[dict]):
+    """
+    Tulostaa vahvistettujen ja ei-vahvistettujen varausten lukumäärän.
+    """
     vahvistetutVaraukset = 0
     eiVahvistetutVaraukset = 0
     for varaus in varaukset:
@@ -65,6 +98,9 @@ def varausten_lkm(varaukset: list[dict]):
     print()
 
 def varausten_kokonaistulot(varaukset: list[dict]):
+    """
+    Laskee ja tulostaa vahvistettujen varausten kokonaistulot.
+    """
     varaustenTulot = 0
     for varaus in varaukset:
         if(varaus['Vahvistettu']):
@@ -74,6 +110,9 @@ def varausten_kokonaistulot(varaukset: list[dict]):
     print()
 
 def main():
+    """
+    Ohjelman pääfunktio. Lukee varaukset varaukset.txt tiedostosta ja tulostaa
+    """
     varaukset = hae_varaukset("varaukset.txt")
     print("1) Vahvistetut varaukset")
     vahvistetut_varaukset(varaukset)
