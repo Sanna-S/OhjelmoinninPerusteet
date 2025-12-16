80b5e2880c80c72d3185718814f72fa2ca24bec1
Sanna-S <sanna.sjoblad@centria.fi>
2025-12-16 22:01:59 +0200

    joko_toimisi

--- Viikko6/viikko6_tehtava.py
+++ Viikko6/viikko6_tehtava.py
@@ -42,7 +42,7 @@ def lue_date(tiedoston_nimi: str) -> list:
 
     return tietokanta
 
-def raportti_tiedostoon(raportti: str,):
+def raportti_tiedostoon(raportti: str):
     """
     Kirjoittaa raportin tiedostoon raportti.txt.
     Parametrit:
@@ -51,19 +51,20 @@ def raportti_tiedostoon(raportti: str,):
     with open("raportti.txt", "w", encoding="utf-8") as f:
         f.write(raportti)
 
+
 def muotoile_raportti(otsikko: str, kulutus: float, tuotanto: float, lampotila: float, tietue_lkm: int) -> str:
     """"
     Muotoilee raportin annetulla otsikolla ja arvoilla.
     """
     raportti = "-" * 50 + "\n"
     raportti += f"{otsikko}\n"
-    raportti += "-Kokonaiskulutus: " + f"{kulutus:.2f}".replace(".", ",") + " kWh\n"
-    raportti += "-Kokonaistuotanto: " + f"{tuotanto:.2f}".replace(".", ",") + " kWh\n"
-    raportti += "-Keskilämpötila: " + f"{lampotila/tietue_lkm:.2f}".replace(".", ",") + " °C\n"
+    raportti += f"-Kokonaiskulutus: " + f"{kulutus:.2f} kWh\n".replace(".", ",")
+    raportti += f"-Kokonaistuotanto: " + f"{tuotanto:.2f} kWh\n".replace(".", ",")
+    raportti += f"-Keskilämpötila: " + f"{lampotila/tietue_lkm:.2f} °C\n".replace(".", ",")
     raportti += "-" * 50 + "\n"
     return raportti
 
-def raportti_aikavali(alkupaiva: datetime.date, loppupaiva: datetime.date, tietokanta: list) -> str:
+def raportti_aikavali(alkupaiva: str, loppupaiva: str, tietokanta: list) -> str:
     """
     Raportiin tulostetaan aikaväliltä:
     - Alkupäivämäärä ja loppupäivämäärä (pp.kk.vvvv-pp.kk.vvvv)
@@ -71,15 +72,23 @@ def raportti_aikavali(alkupaiva: datetime.date, loppupaiva: datetime.date, tieto
     - Aikavälin kokonaistuotannon (kWh, 2 desimaalin tarkkuudella, pilkku desimaalierottimena)
     - Aikavälin keskilämpötila (°C, esim. kaikkien tuntien lämpötilojen keskiarvo)
     Parametrit:
-    raportti (str): raporttiteksti
+    raportti (str): 
     """
+    alkupv = int(alkupaiva.split(".")[0])
+    alkukk = int(alkupaiva.split(".")[1])
+    alkuvv = int(alkupaiva.split(".")[2])   
+    alku = date(alkuvv, alkukk, alkupv)
+    loppupv = int(loppupaiva.split(".")[0])
+    loppukk = int(loppupaiva.split(".")[1])   
+    loppuvv = int(loppupaiva.split(".")[2])
+    loppu = date(loppuvv, loppukk, loppupv)
     kulutus = 0
     tuotanto = 0
     lampotila = 0 
     tietue_lkm = 0 
 
     for tietue in tietokanta:
-        if alkupaiva <= tietue[0].date() <= loppupaiva:
+        if alku <= tietue[0].date() <= loppu:
             kulutus += tietue[1]
             tuotanto += tietue[2]
             lampotila += tietue[3]
@@ -88,7 +97,7 @@ def raportti_aikavali(alkupaiva: datetime.date, loppupaiva: datetime.date, tieto
     otsikko = f"Raportti aikaväliltä: {alkupaiva}-{loppupaiva}"
     return muotoile_raportti(otsikko, kulutus, tuotanto, lampotila, tietue_lkm)
 
-def raportti_kk(kuukausi: int, tietokanta: list) -> str:
+def raportti_kk(kuukausi: str, tietokanta: list) -> str:
     """
     Raporttiin tulostetaan:
     - Kuukausi 
@@ -101,6 +110,7 @@ def raportti_kk(kuukausi: int, tietokanta: list) -> str:
     """
     kuukaudet = ["Tammikuu", "Helmikuu", "Maaliskuu", "Huhtikuu", "Toukokuu", "Kesäkuu","Heinäkuu", "Elokuu", "Syyskuu", "Lokakuu", "Marraskuu", "Joulukuu"]
     
+    kuukausi = int(kuukausi)
     kulutus = 0
     tuotanto = 0    
     lampotila = 0
@@ -142,111 +152,55 @@ def raportti_vuosi(tietokanta: list) -> str:
     otsikko = f"Raportti vuodelta 2025\n"
     return muotoile_raportti(otsikko, kulutus, tuotanto, lampotila, tietue_lkm)
 
-def valikot (paavalikko: bool, alavalikko: bool) -> list:
+def main():
     """
-    Luo valikot ja palauttaa valinnat listana.
-
-    Parametrit:
-    paavalikko (bool): käynnistetään päävalikko
-    alavalikko (bool): käynnistetään alavalikko
-
-    Palauttaa:
-    valikon_valinnat (str): valikon valinnat yhdistettynä arvoihin
+    Ohjelman pääfunktio: kysyy käyttäjältä inputteja ja tulostaa/vie tiedostoon raportteja
     """
-    while True and paavalikko:
-        print("-" * 50)
+    kulutus_Tuotanto2025 = lue_date("2025.csv")
+
+    while True:
         print("Valitse raporttityyppi:")
         print("1) Päiväkohtainen yhteenveto aikaväliltä")
         print("2) Kuukausikohtainen yhteenveto yhdelle kuukaudelle")
         print("3) Vuoden 2025 kokonaisyhteenveto")
         print("4) Lopeta ohjelma")
-        print("-" * 50)
-        try:
-            valinta = int(input("Anna valinta (numero 1-4):"))
-            if not 1 <= valinta <= 4:
-                raise ValueError
-        except:
-            print("Valintasi on virheellinen. Anna numero välillä 1-4.")
-            continue
 
-        if valinta == 1:
-            try:
-                alku_pvm = input("Anna alku päivämäärä (pp.kk.vvvv): ").split(".")
-                loppu_pvm = input("Anna loppu päivämäärä (pp.kk.vvvv): ").split(".")
-                valinnat = [0, 1, date(int(alku_pvm[2]), int(alku_pvm[1]), int(alku_pvm[0])), date(int(loppu_pvm[2]), int(loppu_pvm[1]), int(loppu_pvm[0]))]
-                break
-            except:
-                print("Päivämäärä on väärässä muodossa. Anna päivämäärä muodossa pp.kk.vvvv. Palataan alkuun.")
-                continue
-
-        elif valinta == 2:
-            try:
-                kuukausi = int(input("Anna kuukausi numerona (1-12): "))
-                valinnat = [0, 2, kuukausi]
-                break
-            except:
-                print("Virheellinen valinta. Kuukausi pitää antaa numerona välillä 1-12. Palataan alkuun.")
-                continue
-
-        elif valinta == 3:
-            valinnat = [0, 3]
-            break
-        elif valinta == 4:
-            valinnat = [0, 4]
+        ensimmainen_valinta = int(input("Anna valinta (numero 1-4): "))
+        if ensimmainen_valinta == 1:
+            alkupaiva = input("Anna alku päivämäärä (pp.kk.vvvv): ")
+            loppupaiva = input("Anna loppu päivämäärä (pp.kk.vvvv): ")
+            raportti = raportti_aikavali(alkupaiva, loppupaiva, kulutus_Tuotanto2025)
+            print(raportti)
+        elif ensimmainen_valinta == 2:
+            kuukausi = (input("Anna kuukausi numerona (1-12): "))
+            raportti = raportti_kk(kuukausi, kulutus_Tuotanto2025)
+            print(raportti)
+        elif ensimmainen_valinta == 3:
+            raportti = raportti_vuosi(kulutus_Tuotanto2025)
+            print(raportti)
+        elif ensimmainen_valinta == 4:
+            print("Ohjelma lopetettu.")
             break
         else:
             continue
 
-    while True and alavalikko:
-        print("-" * 50)
         print("Mitä haluat tehdä seuraavaksi?")
         print("1) Kirjoita raportti tiedostoon raportti.txt")
         print("2) Luo uusi raportti")
         print("3) Lopeta")
-        print("-" * 50)
-        try:
-            valinta = int(input("Anna valinta (numero 1-3):"))
-            if not 1 <= valinta <= 3:
-                raise ValueError
-        except: 
-            print("Valintasi on virheellinen. Anna numero välillä 1-3.")
-            continue
-        valinnat = [1, valinta]
-        break
-    
-    return valinnat
-
-    
-
-def main():
-    """
-    Ohjelman pääfunktio: kysyy käyttäjältä inputteja ja tulostaa/vie tiedostoon raportteja
-    """
-    
-    kulutus_Tuotanto2025 = lue_date("2025.csv")
-
-    while True:
-        paavalikko = valikot(True, False)
-        if paavalikko[1] == 1:
-            raportti = raportti_aikavali(paavalikko[2], paavalikko[3], kulutus_Tuotanto2025)
-            print(raportti)
-        elif paavalikko[1] == 2:
-            raportti = raportti_kk(paavalikko[2], kulutus_Tuotanto2025)
-            print(raportti)
-        elif paavalikko[1] == 3:
-            raportti = raportti_vuosi(kulutus_Tuotanto2025)
-            print(raportti)
-        elif paavalikko[1] == 4:
-            break
-
-        alavalikko = valikot(False, True)
-        if alavalikko[1] == 1:
+        toinen_valinta = int(input("Anna valinta (numero 1-3):"))
+        if toinen_valinta == 1:
             raportti_tiedostoon(raportti)
+            print("Raportti kirjoitettu tiedostoon.")
+        elif toinen_valinta == 2:
             continue
-        elif alavalikko[1] == 2:
-            continue
-        elif alavalikko[1] == 3:
+        elif toinen_valinta == 3:
+            print("Ohjelma lopetettu.")
             break
+        else:
+            continue
+
+        print("---------------------------------------------------")       
 
 if __name__ == "__main__":
     main()
